1385346237f0fe2a3fdd914db0d98ecd21042bdd
Takasur Azeem <takasurazeem@gmail.com>
2022-02-25 09:00:53 +0500

    Initial Commit
    Signed-off-by: Takasur Azeem <takasurazeem@gmail.com>

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Takasur Azeem on 25/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
